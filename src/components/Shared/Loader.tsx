db9262e6e90062fbf0700f5e79da65b4a88f1681
maks <maks@pop-os.localdomain>
2020-10-04 21:07:41 +0200

    feat: :lipstick: Add Spinner Loader sizes

--- src/components/Shared/Loader.tsx
+++ src/components/Shared/Loader.tsx
@@ -3,14 +3,15 @@ import React from 'react';
 interface IProps {
   space?: number;
   unit?: 'px' | 'vh' | 'rem';
+  size?: 'tiny' | 'small' | 'normal' | 'large';
 }
 
 export default function Loader(props: IProps) {
-  const { space = 50, unit = 'px' } = props;
+  const { size = 'normal', space = 50, unit = 'px' } = props;
   return (
     <div
       style={{ marginTop: space + unit, marginBottom: space + unit }}
-      className="spinner-loader"
+      className={`spinner-loader spinner-loader--${size}`}
     >
       Loading...
     </div>
