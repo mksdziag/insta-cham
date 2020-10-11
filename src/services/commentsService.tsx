ea40f05fb64ec39228162e6a7c5ac076ca955df6
maks <maks@pop-os.localdomain>
2020-10-11 10:15:04 +0200

    refactor: :fire: Remove console log

--- src/services/commentsService.tsx
+++ src/services/commentsService.tsx
@@ -3,7 +3,6 @@ import { Comments } from '../fakeData/comments';
 import { Comment } from '../interfaces/Comment';
 
 export const getCommentsToPost = async (postId: string) => {
-  console.log(postId);
   const found: Comment[] = await asyncFaker(
     () => Comments.filter((c: Comment) => c.to === postId),
     900
